380ebc8789000a86b0bf06147040444923bffd06
ParichayChuahan12 <parichayrajput2626@gmail.com>
2025-11-15 12:24:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,25 +11,41 @@
     loadAllSongs(singerName);
  }
 
- function createCard(songObject){
+function createCard(songObject){
     const songDiv = document.querySelector('.songs');
+
     const cardDiv = document.createElement('div');
+
     const image = document.createElement('img');
     image.src = songObject.artworkUrl100;
     cardDiv.appendChild(image);
+
     const titleDiv = document.createElement('div');
     titleDiv.innerText = songObject.collectionName;
     cardDiv.appendChild(titleDiv);
+
     const artistDiv = document.createElement('div');
     artistDiv.innerText = songObject.artistName;
     cardDiv.appendChild(artistDiv);
+
     const audioTag = document.createElement('audio');
     audioTag.src = songObject.previewUrl;
     audioTag.controls = true;
+
+    // Pause all other audios when this one starts
+    audioTag.addEventListener('play', () => {
+        const audios = document.querySelectorAll('audio');
+        audios.forEach(audio => {
+            if(audio !== audioTag){
+                audio.pause();
+            }
+        });
+    });
+
     cardDiv.appendChild(audioTag);
     songDiv.appendChild(cardDiv);
+}
 
- }  
  
  async function loadAllSongs(singerName ){
             const URL = `https://itunes.apple.com/search?term=${singerName}&limit=50`;
